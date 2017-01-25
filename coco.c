691941cc95f25a8092e049a0b2b415bbdbad24ec
LF <francuccilea@gmail.com>
2017-01-25 20:08:13 -0300

    added coco.c

---
+++ coco.c
@@ -0,0 +1,13 @@
+/**
+ *  \file       coco.c
+ *  \ingroup    sch
+ *
+ *  \brief      Implements a native, simple and cooperative scheduler called 
+ *              RKS to deal with multiple event - driven state machines.
+ */
+
+void
+rkh_fwk_init(void)
+{
+	printf("Hello");
+}
